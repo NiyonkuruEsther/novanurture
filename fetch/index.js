6a7a3512ea0007d02f5659cc5eac5d684cd09612
dusengepeggy <dickinepeggy100@gmail.com>
2024-04-05 20:02:11 +0200

    making the reset password ,post and adding functionalities

--- fetch/index.js
+++ fetch/index.js
@@ -1,10 +1,39 @@
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import { addDoc, collection, doc, getDocs,updateDoc } from "firebase/firestore";
 import { firestoreDB } from "../FirebaseConfig";
 
 export const writeUserData = async (data) => {
   console.log(data);
   await addDoc(collection(firestoreDB, "users"), { ...data });
 };
+
+export const createPost= async (data)=>{
+  console.log(data);
+  await addDoc(collection(firestoreDB, "posts"), { ...data });
+}
+
+export const readPosts = async () => {
+  let posts=[];
+  await getDocs(collection(firestoreDB, "posts"))
+  .then(
+    (res) => {
+      res.forEach((doc) => {
+        posts.push({ ...doc.data()});
+      });
+    }
+  ).catch(()=>{
+    console.error("Error getting posts: ", error);
+  }
+  )
+  return posts;
+}
+
+export const editUser=async(id,data)=>{
+ 
+  console.log(data);
+  await updateDoc(doc(firestoreDB, "users", id), { ...data });
+  await localStorage.setItem("user", JSON.stringify({id:id,...data}));
+
+}
 
 export const readUsers = async () => {
   let arr = [];
